463a3a678d4a2bf5a73135246c436c418bb6c2c2
Landy Rakotoarison <landytiaa@gmail.com>
2025-08-28 14:29:32 -0400

    Added basic cli

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::cout << "Hello, citybits storage engine!" << std::endl;
-   
+    std::string action_command;
+
+    for (;;) {
+        std::getline(std::cin, action_command);
+        if (!std::cin) break;          
+        if (action_command == "exit") break;
+
+        if (action_command == "zone") {
+            std::cout << "zone command" << std::endl;
+        } else if (action_command == "build") {
+            std::cout << "build command" << std::endl;
+        } else if (action_command == "del") {
+            std::cout << "del command" << std::endl;
+        } else if (!action_command.empty()) {
+            std::cout << "unknown command: " << action_command << std::endl;
+        }
+    }
+
     return 0;
 }
